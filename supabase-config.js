5335577ef46653dfcf556c7ff613813a9fc0fe07
tradingclone321-stack <tradingclone321@gmail.com>
2026-08-13 14:24:51 +0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -6,5 +6,5 @@
 //      anon public key -> SUPABASE_ANON_KEY
 // 3) Paste below. Then run schema.sql in the SQL Editor.
 // ============================================================
-var SUPABASE_URL = 'https://eeniyavnykxqsmpjxesy.supabase.co/rest/v1/';
+var SUPABASE_URL = 'https://eeniyavnykxqsmpjxesy.supabase.co';
 var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlbml5YXZueWt4cXNtcGp4ZXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MTczNDAsImV4cCI6MjEwMjA5MzM0MH0.bK7eQy-XDiKTfoVTn5WDiUmHyYcp5nhozTEyPh9zQKE';
